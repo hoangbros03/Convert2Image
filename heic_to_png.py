dae83bb96f08b0ec587f3f73f3969bbb23dc982f
Tammy Trinh <trinhthiminhtamvq@gmail.com>
2023-07-12 19:10:27 +0700

    Update convert .heic to .png

--- heic_to_png.py
+++ heic_to_png.py
@@ -1,13 +1,17 @@
-from pathlib import Path
-from PIL import Image
 import os
-import pillow_heif
+from pathlib import Path
 
-class Handler():
+import pillow_heif
+from PIL import Image
+
+
+class Handler:
     DEFAULT_EXTENSION = ".png"
     SUPPORT_EXTENSIONS = None
+
     def __init__(self):
         pass
+
     def check_extension(cls, filename):
         ext = Path(filename).suffix
         return ext == cls.SUPPORT_EXTENSIONS
@@ -22,35 +26,37 @@ class Handler():
             heif_file.size,
             heif_file.data,
             "raw",
-            )
-        new_name = file_path.split("\\")[-1]  
-        new_name = new_name.replace('heic', 'png')
-        
-        new_name = new_name.replace('HEIC', 'png')
+        )
+        new_name = file_path.split("\\")[-1]
+        new_name = new_name.replace("heic", "png")
+
+        new_name = new_name.replace("HEIC", "png")
         new_filepath = os.path.join(output_dir, new_name)
-        data.save(new_filepath, format('png'))
+        data.save(new_filepath, format("png"))
 
     def checkDir(self, output_dir):
         # Check if dir exist, otherwise create new
         if not os.path.exists(output_dir):
             os.makedirs(output_dir, exist_ok=True)
-    def save(self, img, prefix_name,output_dir):
-        img.save(Path(".")/output_dir/prefix_name+Handler.DEFAULT_EXTENSION)
 
-if __name__ == '__main__':
-    file_path = r'c:\Users\Tammy\demo\heic-image-converter\test\heic\IMG_1919.HEIC'
-    output_dir = r'c:\Users\Tammy\demo\heic-image-converter\test\output'
-    
+    def save(self, img, prefix_name, output_dir):
+        img.save(Path(".") / output_dir / prefix_name + Handler.DEFAULT_EXTENSION)
+
+
+if __name__ == "__main__":
+    file_path = r"c:\Users\Tammy\demo\heic-image-converter\test\heic\IMG_1919.HEIC"
+    output_dir = r"c:\Users\Tammy\demo\heic-image-converter\test\output"
+
     heif_file = pillow_heif.read_heif(file_path)
     data = Image.frombytes(
         heif_file.mode,
         heif_file.size,
         heif_file.data,
         "raw",
-        )
-    new_name = file_path.split("\\")[-1]  
-    new_name = new_name.replace('heic', 'png')
-    
-    new_name = new_name.replace('HEIC', 'png')
+    )
+    new_name = file_path.split("\\")[-1]
+    new_name = new_name.replace("heic", "png")
+
+    new_name = new_name.replace("HEIC", "png")
     new_filepath = os.path.join(output_dir, new_name)
-    data.save(new_filepath, format('png'))
+    data.save(new_filepath, format("png"))
