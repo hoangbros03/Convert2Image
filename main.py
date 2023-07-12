791da66d9bb2b0c3ccd8a208a1ed5c90d0c940c0
doubleK29 <khanh.techbk@gmail.com>
2023-07-12 15:58:26 +0700

    Update argparser

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-import logging
+import argparse
 
-logging.basicConfig(
-    filemode="a",
-    filename="log.log",
-    format="%(asctime)s - %(message)s",
-    level=logging.DEBUG,
-)
-logging.info("Test")
+
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description="Convert file to image")
+    parser.add_argument(
+        "-f",
+        "--image-file",
+        type=str,
+        nargs="+",
+        help="Insert image file that need converting to png",
+    )  # image path argument
+    return parser.parse_args()
+
+
+def main():
+    """Convert file from argument to image
+    Return: A path to a folder containing that image?
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
